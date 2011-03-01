a4754c164e29fe741bceb660ba81f73aa7e0301a
jhardy <jhardy@ilovetocreate.com>
2011-03-01 09:44:16 -0800

    Initial commit

---
+++ templates/project/manifest.rb
@@ -0,0 +1,11 @@
+stylesheet '_omgtext.sass', :media => 'screen, projection'
+
+description "ridiculous css3 text shadows"
+
+help %Q{
+Installs a html file and stylesheet that you can use as a reference or use as a base for your project
+}
+
+welcome_message %Q{
+Refer to the stylesheet and html file for useage examples 
+}
