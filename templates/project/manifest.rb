e8f73a820bf773ddddb574293a29dce2dcb57e43
Jared Hardy <jared@jaredhardy.com>
2011-05-08 22:57:50 -0700

    Added offset text shadow style and updated angle shortcut alias

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,11 +1,11 @@
-stylesheet '_omgtext.sass', :media => 'screen, projection'
+stylesheet 'omgtext.sass', :media => 'screen, projection'
 
 description "ridiculous css3 text shadows"
 
 help %Q{
-Installs a html file and stylesheet that you can use as a reference or use as a base for your project
+Installs a stylesheet that you can use as a reference for usage examples
 }
 
 welcome_message %Q{
-Refer to the stylesheet and html file for useage examples 
+Refer to the stylesheet file for usage examples 
 }
